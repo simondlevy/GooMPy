1912b6c77e0fdb63949a3c12e5c3e3b8d2a219e4
Simon D. Levy <simon.d.levy@gmail.com>
2015-06-26 21:01:32 -0400

    Update __init__.py

--- goompy/__init__.py
+++ goompy/__init__.py
@@ -1,5 +1,8 @@
 '''
+GooMPy: Google Maps for Python
+
 Copyright (C) 2015 Alec Singer and Simon D. Levy
+
 This code is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as 
 published by the Free Software Foundation, either version 3 of the 
